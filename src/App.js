cfdc4c158cd5b92b5444aa52b926023c9a5182aa
karibasava <karibasava.t.g@gmail.com>
2021-08-02 14:45:41 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-import React from 'react';
-
-import Main from './Pages/Main';
+import './App.css';
+import BalanceCards from './components/BalanceCard/BalanceCards';
 import Header from './components/Header/Header';
+import Path from './components/Path/Path';
+import PageHeading from './components/PageHeading/PageHeading';
+import Pageinfo from './components/PageInfo/Pageinfo';
 import Footer from './components/Footer/Footer';
+import Buttons from './components/Buttons/Buttons';
+import Slider from './components/Slider/Slider';
 
-const APP = () => {
-  return ( 
+
+
+function App() {
+  return (
     <div className="App">
+      
       <Header />
-      <Main />
+      <Path path="Home > Account Overview" />
+      <PageHeading data ="Account Overview" />
+      <Pageinfo />
+      <BalanceCards />
+      <Buttons />
+      <Slider />
       <Footer />
     </div>
-   );
+  );
 }
- 
-export default APP;
+
+export default App;
